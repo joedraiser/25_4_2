470dea26161542100fd22ac13162a95341d9ba9c
Evgenii.Korchagov <joedraiser@gmail.com>
2021-12-02 18:02:28 +0300

    Initial commit

---
+++ Source/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "cpu.h"
+#include "disk.h"
+#include "gpu.h"
+#include "kbd.h"
+#include "ram.h"
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
